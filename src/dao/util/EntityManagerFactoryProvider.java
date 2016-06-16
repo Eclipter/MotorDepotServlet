d9b029d43e677c1fe024fc03a9697dfd00e0a491
Eclipter <eclipter97@gmail.com>
2016-06-16 23:55:58 +0300

    Removed PersistenceUnit properties from xml file

--- src/dao/util/EntityManagerFactoryProvider.java
+++ src/dao/util/EntityManagerFactoryProvider.java
@@ -5,6 +5,8 @@ import util.DatabaseConfigurationParameterName;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Singleton used to provide EntityManagerFactory to DAO classes
@@ -13,8 +15,12 @@ import javax.persistence.Persistence;
 public class EntityManagerFactoryProvider {
 
     private static final String PERSISTENCE_UNIT_NAME = "MotorDepotHibernateManager";
+    private static final String URL_PROPERTY = "hibernate.connection.url";
+    private static final String DRIVER_PROPERTY = "hibernate.connection.driver_class";
+    private static final String USERNAME_PROPERTY = "hibernate.connection.username";
+    private static final String PASSWORD_PROPERTY = "hibernate.connection.password";
 
-    private static final EntityManagerFactoryProvider OUR_INSTANCE = new EntityManagerFactoryProvider();
+    private static final EntityManagerFactoryProvider INSTANCE = new EntityManagerFactoryProvider();
 
     private final EntityManagerFactory factory;
 
@@ -23,13 +29,22 @@ public class EntityManagerFactoryProvider {
     }
 
     public static EntityManagerFactoryProvider getInstance() {
-        return OUR_INSTANCE;
+        return INSTANCE;
     }
 
     private EntityManagerFactoryProvider() {
         if(Boolean.parseBoolean(
                 DatabaseConfigurationBundleManager.getProperty(DatabaseConfigurationParameterName.USE_JPA))) {
-            this.factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+            String url = DatabaseConfigurationBundleManager.getProperty(DatabaseConfigurationParameterName.URL);
+            String driver = DatabaseConfigurationBundleManager.getProperty(DatabaseConfigurationParameterName.DRIVER);
+            String userName = DatabaseConfigurationBundleManager.getProperty(DatabaseConfigurationParameterName.USERNAME);
+            String password = DatabaseConfigurationBundleManager.getProperty(DatabaseConfigurationParameterName.PASSWORD);
+            Map<String, String> properties = new HashMap<>();
+            properties.put(URL_PROPERTY, url);
+            properties.put(DRIVER_PROPERTY, driver);
+            properties.put(USERNAME_PROPERTY, userName);
+            properties.put(PASSWORD_PROPERTY, password);
+            this.factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME, properties);
         }
         else {
             this.factory = null;
